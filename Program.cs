97ea27251e6ef119dc03943fe9f71b80e711fded
ADMIN <ADMIN@LAB1-MAY17>
2023-09-04 14:01:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten cua ban: ");
+            string ten = Console.ReadLine();
+            Console.WriteLine("Hello suc vat {0}!", ten);
+            Console.ReadKey();
+        }
+    }
+}
